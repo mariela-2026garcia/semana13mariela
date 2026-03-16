0a8fc07fc58ad75dd0b57f33f0f1006b16826e20
mariela-2026garcia <marielastefaniag2026@gmail.com>
2026-03-15 23:32:03 -0500

    mi tercer proyecto origen

--- src/semana13mariela/Semana13mariela.java
+++ src/semana13mariela/Semana13mariela.java
@@ -14,7 +14,13 @@ public class Semana13mariela {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+     double nota1 = 7; 
+         double nota2 = 9;
+         double resultado = calcularPromedio(nota1, nota2);
+         System.out.println("El promedio final es: " + resultado);
+        }
+         public static double calcularPromedio(double nota1, double nota2) {
+         double promedio = (nota1 + nota2) / 2;
+         return promedio;
     }
-    
 }
